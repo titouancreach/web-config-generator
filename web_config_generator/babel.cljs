79a4ceccd3c90106e4189c7175d9948c13d734c7
Titouan CREACH <titouan.creach@eegle.io>
2018-07-18 22:25:56 +0200

    Indent using calva-fmt

--- web_config_generator/babel.cljs
+++ web_config_generator/babel.cljs
@@ -1,18 +1,18 @@
 (ns web-config-generator.babel)
 
 (defn add-in
-    [baseconf, key, elem]
-    (let [target (get-in baseconf [:babel, key])]
-        (assoc-in baseconf [:babel, key] (conj target elem))))
+  [baseconf, key, elem]
+  (let [target (get-in baseconf [:babel, key])]
+    (assoc-in baseconf [:babel, key] (conj target elem))))
 
 (defn plugin
-    "Add a babel plugin"
-    [name & {:keys [config] :or {config {}}}]
-    (fn [baseconf]
-        (add-in baseconf :plugins [name, config])))
+  "Add a babel plugin"
+  [name & {:keys [config] :or {config {}}}]
+  (fn [baseconf]
+    (add-in baseconf :plugins [name, config])))
 
 (defn preset
-    "Add a babel preset"
-    [name & {:keys [config] :or {config {}}}]
-    (fn [baseconf]
-        (add-in baseconf :presets [name, config])))
+  "Add a babel preset"
+  [name & {:keys [config] :or {config {}}}]
+  (fn [baseconf]
+    (add-in baseconf :presets [name, config])))
--- web_config_generator/core.cljs
+++ web_config_generator/core.cljs
@@ -1,28 +1,28 @@
 (ns web-config-generator.core
-    (:require ["web-config-generator.babel" :as babel]
-              ["cljs.pprint"]))
+  (:require ["web-config-generator.babel" :as babel]
+            ["cljs.pprint"]))
 
 (def baseconf
-    "Base configuration"
-    {:env {} :babel {:plugins [] :presets []}})
+  "Base configuration"
+  {:env {} :babel {:plugins [] :presets []}})
 
 (defn env
-    "Add an environment variable to the config"
-    [name value]
-    (fn [config] (assoc-in config [:env, name] value)))
+  "Add an environment variable to the config"
+  [name value]
+  (fn [config] (assoc-in config [:env, name] value)))
 
 (defn run
-    "Compose and execute all functions defined in run"
-    [baseconf & args]
-    ((apply comp args) baseconf))
+  "Compose and execute all functions defined in run"
+  [baseconf & args]
+  ((apply comp args) baseconf))
 
 (def run-with-baseconf (partial run baseconf))
 
 (cljs.pprint/pprint
-    (run-with-baseconf
-        (env "PORT" 9000)
-        (env "TEST" "Titouan")
-        (babel/plugin "jsx-transformer2" :config {:loose "titouan2"})
-        (babel/plugin "Test")
-        (babel/preset "react-preset" :config {:test "titouan"})))
+ (run-with-baseconf
+  (env "PORT" 9000)
+  (env "TEST" "Titouan")
+  (babel/plugin "jsx-transformer2" :config {:loose "titouan2"})
+  (babel/plugin "Test")
+  (babel/preset "react-preset" :config {:test "titouan"})))
 
